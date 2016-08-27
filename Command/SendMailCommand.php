32e6e6db628c24040e8c724887a0d5169fb0c181
Jimmy Tournemaine <jimmy.tournemaine@yahoo.fr>
2016-08-27 15:19:45 +0200

    Bug fix

--- Command/SendMailCommand.php
+++ Command/SendMailCommand.php
@@ -4,6 +4,7 @@ namespace JT\MailBundle\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 
 class SendMailCommand extends Command
 {
