c9be36d15f21c2e754c57b472593a26b19029080
Mathieu Zonzon <mathieu.zonzon@groupe-asten.fr>
2016-08-27 09:22:59 +0200

    Add sending test

---
+++ Command/SendMailCommand.php
@@ -0,0 +1,29 @@
+<?php
+namespace JT\MailBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SendMailCommand extends Command
+{
+	protected function configure()
+	{
+		$this
+			->setName('jt:mail:send')
+			->setDescription('Send a mail')
+			->setHelp("This command allows you to test a mail sending.")
+			->addArgument('to', InputArgument::REQUIRED, 'To who we have to send the message.')
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$sender = $this->getContainer()->get('jt_mail.test.sending');
+		$to = $intput->getArgument('to');
+
+		$sender->send();
+		$output->writeln("Message sent to $to.");
+	}
+}
+
